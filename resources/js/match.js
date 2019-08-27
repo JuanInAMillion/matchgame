f1ec21a78dc223b1d708ac75419f4a16b2819262
Juan A Alcantara <juan231f@yahoo.com>
2019-08-26 22:58:22 -0400

    updated match.js variables and title elements

--- resources/js/match.js
+++ resources/js/match.js
@@ -1,33 +1,33 @@
-const MatchGame = {};
+const Match = {};
 
 /*
   Sets up a new game after HTML document has loaded.
   Renders a 4x4 board of cards.
 */
 
-$(document).ready(function() {
+$(document).ready(function () {
   const $game = $('#game');
-  const values = MatchGame.generateCardValues();
-  MatchGame.renderCards(values, $game);
+  const values = Match.generateCards();
+  Match.renderCards(values, $game);
 });
 
 /*
   Generates and returns an array of matching card values.
  */
 
-MatchGame.generateCardValues = function () {
-  const sequentialValues = [];
+Match.generateCards = function () {
+  const cardSequence = [];
 
   for (let value = 1; value <= 8; value++) {
-    sequentialValues.push(value);
-    sequentialValues.push(value);
+    cardSequence.push(value);
+    cardSequence.push(value);
   } // [1, 1, 2, 2, ...]
 
   const cardValues = [];
 
-  while (sequentialValues.length > 0) {
-    let randomIndex = Math.floor(Math.random() * sequentialValues.length);
-    let randomValue = sequentialValues.splice(randomIndex, 1)[0];
+  while (cardSequence.length > 0) {
+    let randomIndex = Math.floor(Math.random() * cardSequence.length);
+    let randomValue = cardSequence.splice(randomIndex, 1)[0];
     cardValues.push(randomValue);
   }
 
@@ -40,7 +40,7 @@ MatchGame.generateCardValues = function () {
   object.
 */
 
-MatchGame.renderCards = function(cardValues, $game) {
+Match.renderCards = function (cardValues, $game) {
   const colors = [
     'hsl(50, 85%, 65%)',
     'hsl(100, 85%, 65%)',
@@ -65,11 +65,11 @@ MatchGame.renderCards = function(cardValues, $game) {
       color: color,
     };
     $cardElement.data(data);
-  
+
     $game.append($cardElement);
   }
-  $('.card').click(function() {
-    MatchGame.flipCard($(this), $('#game'));
+  $('.card').click(function () {
+    Match.flipCard($(this), $('#game'));
   })
 
 };
@@ -79,40 +79,40 @@ MatchGame.renderCards = function(cardValues, $game) {
   Updates styles on flipped cards depending whether they are a match or not.
  */
 
-MatchGame.flipCard = function($card, $game) {
+Match.flipCard = function ($card, $game) {
   if ($card.data('isFlipped')) {
     return;
   }
   $card.css('background-color', $card.data('color'))
-  .text($card.data('value'))
-  .data('isFlipped', true);
+    .text($card.data('value'))
+    .data('isFlipped', true);
 
   let flippedCards = $game.data('flippedCards');
   flippedCards.push($card);
 
-  let card1 = flippedCards[0];
-  let card2 = flippedCards[1];
+  let firstCard = flippedCards[0];
+  let secondCard = flippedCards[1];
 
-  if(flippedCards.length === 2) {
-    if(card1.data('value') === card2.data('value')) {
+  if (flippedCards.length === 2) {
+    if (firstCard.data('value') === secondCard.data('value')) {
       const matchCSS = {
         backgroundColor: 'rgb(153, 153, 153)',
         color: 'rgb(204, 204, 204)',
       }
 
-      card1.css(matchCSS);
-      card2.css(matchCSS);
+      firstCard.css(matchCSS);
+      secondCard.css(matchCSS);
     } else {
-      
-      window.setTimeout(function(){
-        card1.css('background-color', 'rgb(73, 162, 112)')
-      .text('')
-      .data('isFlipped', false);
-      card2.css('background-color', 'rgb(73, 162, 112)')
-      .text('')
-      .data('isFlipped', false);
+
+      window.setTimeout(function () {
+        firstCard.css('background-color', 'rgb(73, 162, 112)')
+          .text('')
+          .data('isFlipped', false);
+        secondCard.css('background-color', 'rgb(73, 162, 112)')
+          .text('')
+          .data('isFlipped', false);
       }, 500)
-      
+
     }
 
     $game.data('flippedCards', []);
